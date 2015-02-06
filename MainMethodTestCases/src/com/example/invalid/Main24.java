4df478511c685aae1bf70424daf5c8d8c517f4f8
benjholla <benjholla@gmail.com>
2015-02-06 14:01:54 -0600

    added another test case

---
+++ MainMethodTestCases/src/com/example/invalid/Main24.java
@@ -0,0 +1,14 @@
+package com.example.invalid;
+
+public class Main24 {
+
+	/**
+	 * Invalid.
+	 * 
+	 * Main methods must take a single String array as a parameter
+	 */
+	public static void main(String[] args, String[] args2) {
+
+	}
+
+}
