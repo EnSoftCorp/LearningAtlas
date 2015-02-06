ffb414a66a0d4e40283d1f079626efbee718b046
benjholla <benjholla@gmail.com>
2015-02-06 13:50:59 -0600

    added another testcase

---
+++ MainMethodTestCases/src/com/example/invalid/Main23.java
@@ -0,0 +1,14 @@
+package com.example.invalid;
+
+public class Main23 {
+
+	/**
+	 * Invalid.
+	 * 
+	 * Main methods must take a single String array as a parameter
+	 */
+	public static void main() {
+
+	}
+
+}
